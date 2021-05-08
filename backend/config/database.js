a4cb746449cc9ae1a1d1002aef4c5780fd67afa7
wangheng <1765174487@qq.com>
2021-05-08 16:42:35 +0800

    fix: mysql

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,22 @@
-module.exports = ({env}) => ({
+module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        database: env('MYSQL_DATABASE'),
-        host: env.int('MYSQL_HOST'),
-        port: env('MYSQL_PORT'),
-        username: env('MYSQL_USER'),
-        password: env('MYSQL_PASSWORD')
+        host: env('MYSQL_HOST', 'localhost'),
+        port: env.int('MYSQL_PORT', 5432),
+        database: env('MYSQL_DATABASE', 'strapi'),
+        username: env('MYSQL_USER', 'strapi'),
+        password: env('MYSQL_PASSWORD', 'strapi'),
+        // schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        // ssl: {
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        // },
       },
       options: {
-        useNullAsDefault: true,
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
